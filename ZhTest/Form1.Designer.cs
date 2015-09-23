27c7892f9553e628176f191b16008b9eccf2bd11
IoRyY <253625488@qq.com>
2015-09-23 15:11:51 +0800

    ceshi

--- ZhTest/Form1.Designer.cs
+++ ZhTest/Form1.Designer.cs
@@ -100,7 +100,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ceshi";
             this.ResumeLayout(false);
             this.PerformLayout();
 
